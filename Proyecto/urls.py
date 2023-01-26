351f0270ee4f2f312c83b9a7b1587ecaf8ca5b2f
Daviana Saavedra <davianasaavedra@hotmail.com>
2023-01-26 16:20:02 -0300

    Mymbaeweb

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('AppCoder/',include ('AppCoder.urls'))
+    path("admin/", admin.site.urls),
+    path("",include ('AppCoder.urls'))
+    
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, 
+    document_root=settings.MEDIA_ROOT)
